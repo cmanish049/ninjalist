157fe2c098aa4caec08467e30d9e45c4de1cbbfb
cmanish049 <cmanish049@gmail.com>
2021-04-03 20:07:04 +0545

    final

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,14 @@
 import Link from 'next/link';
-
+import Image from 'next/image';
 export default function Navbar() {
-
     return (
         <nav>
             <div className="logo">
-                <h1>Ninja List</h1>
+                <Image src="/logo.png" width={128} height={77} />
             </div>
             <Link href="/"><a>Home</a></Link>
             <Link href="/about"><a>About</a></Link>
             <Link href="/ninjas"><a>Ninjas</a></Link>
-        </nav>
-        
+        </nav>        
     )
 }
